e0271db39d52fb451a29a385cedaaacc9369aea0
Bryan Romero <bromero52@gmail.com>
2020-04-07 19:02:27 -0400

    Update User model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -2,6 +2,14 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema(
-    
-)
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 4,
+        trim: true
+    },
+}, {
+    timestamps: true,
+});
