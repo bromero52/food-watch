f49bae45bb12d6a070218f314185baf9856a1fe8
bromero1 <bromero52@gmail.com>
2020-04-13 14:17:53 -0400

    getting there

--- src/components/About.js
+++ src/components/About.js
@@ -3,12 +3,25 @@ import '../styles/About.css'
 export default class About extends Component {
     render() {
         return (
-            <div className="container">
+            <div className="about-container">
                 <h1 className="title"> About Food Watch </h1>
 
                 <p>
-                    Food watch was created to help you keep track of 
-                    the foods you eat. 
+                    I'm Bryan! I created Food Watch to help me keep track of 
+                    the foods that I eat. Normally, I would tabulate my food 
+                    consumption on Google Notes. 
+                </p>
+                
+                <p> 
+                    This method worked, but I wanted to use the skills I have
+                    learned while earning a degree in Computer Science to create
+                    a clean interface for this task. 
+                </p>
+
+                <p> 
+                    Building this project gives me the practice I need to develop
+                    and grow my skills that are needed in software engineering that
+                    weren't taught in school. I hope 
                 </p>
             </div>
         )
