8d41db0b9f5a20f9fea3f9d5d1e0dc41c8f298d4
Bryan Romero <bromero52@gmail.com>
2020-08-09 22:53:19 -0400

    ++

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,25 +1,113 @@
-import React, { Component } from 'react';
-import '../styles/Form.css';
+import React, { Component, useState } from "react";
+import { Card, CardBody, Row, Container, Form, Button } from "reactstrap";
+import "../styles/Login.css";
+import "whatwg-fetch";
 
-export default class register extends Component {
-    render() {
-        return (
-            <div>
-                <form className="form">
-                    <h1 className="form-title">Sign Up</h1>
-                <label for=""fname>First Name</label>
-                    <input type="text" id="fname" name="fname"></input>
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-                    <label for="lname">Last Name</label>
-                    <input type="text" id="lname" name="lname"></input>
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
 
-                    <label for="email">Email</label>
-                    <input type="text" placeholder ="Email" id="email" name="email"></input>
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password"></input>
-                    <input type="submit" value="Register"></input>
-                </form>
-            </div>
-        )
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    sub();
+  }
+
+  const data = {
+  };
+
+  const sub = fetch("http://localhost:8000/users/register", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => {
+      console.log("this is res", res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <Card className="card-signin my-5">
+              <CardBody>
+                <h5 className="card-title text-center">Sign Up</h5>
+
+                <Form onSubmit={handleSubmit} className="form-signin">
+                  <div className="form-label-group">
+                    <input
+                      type="email"
+                      id="inputEmail"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      className="form-control"
+                      required
+                      autoFocus
+                    />
+                    <label htmlFor="inputEmail">Email address</label>
+                  </div>
+
+                  <div className="form-label-group">
+                    <input
+                      type="password"
+                      id="inputPassword"
+                      className="form-control"
+                      required
+                    />
+                    <label htmlFor="inputPassword">Password</label>
+                  </div>
+
+                  <div className="custom-control custom-checkbox mb-3">
+                    <input
+                      type="checkbox"
+                      className="custom-control-input"
+                      id="customCheck1"
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="customCheck1"
+                    >
+                      Remember password
+                    </label>
+                  </div>
+
+                  <Button
+                    className="btn btn-lg btn-block text-uppercase"
+                    type="submit"
+                    color="primary"
+                  >
+                    Sign in
+                  </Button>
+                  <hr className="my-4"></hr>
+                  <Button
+                    className="btn-google btn-block text-uppercase"
+                    type="submit"
+                  >
+                    <i className="fab fa-google mr-2"></i> Sign in with Google
+                  </Button>
+                  <Button
+                    className="btn btn-lg btn-facebook btn-block text-uppercase"
+                    type="submit"
+                  >
+                    <i className="fab fa-facebook-f mr-2"></i> Sign in with
+                    Facebook
+                  </Button>
+                </Form>
+              </CardBody>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </div>
+  );
 }
