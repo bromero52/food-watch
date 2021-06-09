6912573244e9d569b258e75269b8361927f06aab
bryan romero <bromero52@gmail.com>
2021-06-09 19:17:34 -0400

    icons

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -1,31 +1,37 @@
 import React from "react";
 import { Layout, Menu, Breadcrumb } from "antd";
 import {
+  LineChartOutlined,
+  MedicineBoxOutlined,
   UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
-  MailOutlined,
-  SettingOutlined,
-  PieChartOutlined,
-  
-} from "antd/lib/icon";
-
-import { Header, Content, Footer, Sider } from "antd/lib/layout";
+} from "@ant-design/icons";
+import { Header, Content, Footer, Sider, } from "antd/lib/layout";
 import SubMenu from "antd/lib/menu/SubMenu";
-
+// import Sider from "antd/lib/layout/Sider"
 export default function Navi() {
   return (
-    <Menu style={{ minHeight: '100vh', maxWidth:'25vh' }} theme="dark" mode="vertical" defaultSelectedKeys={["1"]}>
-      <Menu.Item key="1">Home</Menu.Item>
+    <Sider>
+      <Menu
+        style={{ minHeight: "100vh", maxWidth: "25vh" }}
+        theme="dark"
+        mode="vertical"
+        defaultSelectedKeys={["1"]}
+      >
+        <Menu.Item key="0">Home</Menu.Item>
 
-      <SubMenu icon={<PieChartOutlined/>} title="Meal Plan">
-        <Menu.Item>View Meal Plan</Menu.Item>
-        <Menu.Item>Edit Meal Plan</Menu.Item>
-      </SubMenu>
+        <SubMenu icon={<MedicineBoxOutlined />} title="Meal Plan">
+          <Menu.Item>View Meal Plan</Menu.Item>
+          <Menu.Item>Edit Meal Plan</Menu.Item>
+        </SubMenu>
 
-      <SubMenu icon={<PieChartOutlined />} title="Progress">
-        <Menu.Item>View Progress</Menu.Item>
-      </SubMenu>
-    </Menu>
+        <SubMenu icon={<LineChartOutlined />} title="Progress">
+          <Menu.Item>View Progress</Menu.Item>
+        </SubMenu>
+
+        <Menu.Item key="profile" icon={<UserOutlined />}>
+          My Profile
+        </Menu.Item>
+      </Menu>
+    </Sider>
   );
 }
