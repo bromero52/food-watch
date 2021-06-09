3b32eb4cca38ade67e5eb9ae38c18157f557a26c
Bryan Romero <bryanromero9@gmail.com>
2021-06-09 18:26:39 -0400

    Links in nav

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -1,31 +1,41 @@
 import React from "react";
-import { Layout, Menu, Breadcrumb } from "antd";
-import {
-  UserOutlined,
-  LaptopOutlined,
-  NotificationOutlined,
-  MailOutlined,
-  SettingOutlined,
-  PieChartOutlined,
-  
-} from "antd/lib/icon";
+import { Layout, Menu, Breadcrumb, Sub } from "antd";
 
-import { Header, Content, Footer, Sider } from "antd/lib/layout";
+import { Header, Content, Footer } from "antd/lib/layout";
+import Sider from 'antd/lib/layout/Sider';
 import SubMenu from "antd/lib/menu/SubMenu";
-
+import SubMenuProps from "antd/lib/menu/SubMenu/";
+import PieChartOutlined from "antd/lib/icon/";
+import { Link } from "react-router-dom";
 export default function Navi() {
   return (
-    <Menu style={{ minHeight: '100vh', maxWidth:'25vh' }} theme="dark" mode="vertical" defaultSelectedKeys={["1"]}>
-      <Menu.Item key="1">Home</Menu.Item>
+    <Sider>
+      <Menu
+        style={{ minHeight: "100vh", maxWidth: "35vh" }}
+        theme="dark"
+        mode="vertical"
+        defaultSelectedKeys={["1"]}
+      >
+        <Menu.Item key="1">Home
+        <Link to="/" />
+        </Menu.Item>
 
-      <SubMenu icon={<PieChartOutlined/>} title="Meal Plan">
-        <Menu.Item>View Meal Plan</Menu.Item>
-        <Menu.Item>Edit Meal Plan</Menu.Item>
-      </SubMenu>
+        <SubMenu title="Meal Plan">
+          <PieChartOutlined />
+          <Menu.Item>View Meal Plan</Menu.Item>
+          <Menu.Item>Edit Meal Plan</Menu.Item>
+          <Menu.Item>
+            Add Meal
+            <Link to="/addmeal" className="nav-text">
+              A
+            </Link>
+          </Menu.Item>
+        </SubMenu>
 
-      <SubMenu icon={<PieChartOutlined />} title="Progress">
-        <Menu.Item>View Progress</Menu.Item>
-      </SubMenu>
-    </Menu>
+        <SubMenu title="Progress">
+          <Menu.Item>View Progress</Menu.Item>
+        </SubMenu>
+      </Menu>
+    </Sider>
   );
 }
